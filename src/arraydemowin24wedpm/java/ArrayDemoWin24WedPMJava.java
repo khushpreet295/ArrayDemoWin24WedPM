8c2ff18f9a00ad3a57070be98f6356ec1a878ec9
khush <khush@10.48.52.66>
2024-01-24 13:45:48 -0500

    first commit

---
+++ src/arraydemowin24wedpm/java/ArrayDemoWin24WedPMJava.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraydemowin24wedpm.java;
+
+/**
+ *
+ * @author khush
+ */
+public class ArrayDemoWin24WedPMJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  vjv khush{
+        // TODO code application logic here
+        System.out.println("hello");
+        
+        Scanner input = new Scanner(System.in);
+        
+        String myWorld  = input.nextLine();
+        char[] myLetter = new char[myWorldlength()];
+        for(int i=0; i<myLetters.)
+        
+    }
+    
+}
